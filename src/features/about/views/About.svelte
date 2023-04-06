70d7bf1ddb96f630d3286a73cea362b68ff29d91
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2023-04-06 11:11:44 -0500

    init

---
+++ src/features/about/views/About.svelte
@@ -0,0 +1,31 @@
+<script>
+    
+	import {Link} from 'svelte-routing'
+  import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+  const progress = tweened(0, {
+		duration: 400,
+		easing: cubicOut
+	});
+
+  let inputValue = 0;
+</script>
+
+<p>About works!</p>
+<p>me lavo la carita con agua y con jabon</p>
+<Link href="/">Go home</Link>
+
+<div>
+  <progress value={$progress}></progress>
+</div>
+<input type="number" bind:value={inputValue} on:change={()=> progress.set(inputValue/100)}/>
+
+
+
+<style>
+	progress {
+		display: block;
+		width: 100%;
+	}
+</style>
